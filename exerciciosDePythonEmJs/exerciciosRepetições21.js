248583e8492f4f8f2f7f36f24c8b6d715f2b29b4
Matheus Fantini <matheusfantini@gmail.com>
2022-07-26 20:04:31 -0300

    Create exerciciosRepetições21.js

---
+++ exerciciosDePythonEmJs/exerciciosRepetições21.js
@@ -0,0 +1,29 @@
+num = 11
+for (i = 1; i <= num; i++)
+    lista = []
+divisor = 0
+// lista.forEach(multiplicador => {
+//     if(multiplicador%num==0)
+//     divisor+=1    
+
+// });
+// if(divisor==2){
+//     console.log(`o numero ${num} é primo.`)
+// }
+// else{
+//     console.log(`o número ${num} não é primo`)
+// }
+function primos(num){
+    for (i = 1; i <= num; i++) {
+        //console.log(num % i)
+        if (num % i == 0) {
+            divisor += 1
+        }
+    }
+    primo = (divisor == 2 ? console.log(`o numero ${num} é primo.`) : console.log(`o número ${num} não é primo`))
+    return primo
+}
+
+primos(5)
+
+

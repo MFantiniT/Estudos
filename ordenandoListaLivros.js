5dc32877829f4dc06092fe79009575f2ec0b6ee9
Matheus Fantini <matheusfantini@gmail.com>
2022-07-26 20:12:16 -0300

    Create ordenandoListaLivros.js

---
+++ ordenandoListaLivros.js
@@ -0,0 +1,35 @@
+lista = [25, 15, 20, 30, 18, 12, 9, 40, 42, 54, 84, 11]
+guardarValor = 0
+
+
+lista.sort((a, b) => (a - b))
+
+console.log(lista)
+
+
+
+// for (let i = 0; i < lista.length-1; i++) {
+//     if (lista[i]>lista[i+1]) {
+    //         guardarValor = lista[i]
+    //         lista[i]=lista[i+1]
+    //         lista[i+1]=guardarValor
+//     }
+// }
+// console.log(lista)
+// ListaOrdenada = []
+// lista.forEach(valor => {
+//     for (i = lista.indexOf(valor); i < lista.length; i++) {
+//         if (valor > lista[i]) {
+//             guardarValor = valor
+//             lista.indexOf(valor) = lista[i]
+//         }
+//     }
+
+// });
+// console.log(lista)
+
+
+
+
+
+

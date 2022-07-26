1c7a72ae930e1f74f86cc2f2ae8eb42f5b6d598a
Matheus Fantini <matheusfantini@gmail.com>
2022-07-26 20:03:18 -0300

    Create exerciciosListas9.js

---
+++ exerciciosDePythonEmJs/exerciciosListas9.js
@@ -0,0 +1,15 @@
+// // let nome = "matheus Fantini"
+// // console.log(nome.length)
+
+// Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
+
+function numReverse(num,numReves) {
+    for(i=num.length-1;i>=0;i--){
+        numReves+=num[i]
+    }
+    return console.log(numReves)
+}
+let num = "9876543210"
+let numReves =""
+
+numReverse(num,numReves)

8f9f58922c60ad6d195abbf9777e6396774f7d92
Matheus Fantini <matheusfantini@gmail.com>
2022-07-26 20:02:56 -0300

    Create exerciciosListas3.js

---
+++ exerciciosDePythonEmJs/exerciciosListas3.js
@@ -0,0 +1,12 @@
+// // Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+// [nota1,nota2,nota3,nota4] = [4, 2, 5, 4]
+// console.log( `suas 4 notas são: ${[nota1 ,nota2 ,nota3 ,nota4]} e a sua média é: ${([nota1 +nota2 +nota3 +nota4])/4} `)
+
+notas = [4, 5, 8, 10]
+mediaC=0
+notas.forEach(nota => {
+    mediaC += nota/notas.length
+    
+    
+});
+console.log(mediaC)

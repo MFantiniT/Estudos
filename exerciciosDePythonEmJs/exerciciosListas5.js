4704b6f70b6a383a07b05be439654531b2767b76
Matheus Fantini <matheusfantini@gmail.com>
2022-07-27 21:35:41 -0300

    Update exerciciosListas5.js

--- exerciciosDePythonEmJs/exerciciosListas5.js
+++ exerciciosDePythonEmJs/exerciciosListas5.js
@@ -1,8 +1,8 @@
 //Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 
-numeros = [10,25,32,55,8,9,54,90,54,23,345,7,9,20,87,6,5,,56,87,899,8,7,52]
-par = []
-impar = []
+let numeros = [10,25,32,55,8,9,54,90,54,23,345,7,9,20,87,6,5,,56,87,899,8,7,52]
+let par = []
+let impar = []
 
 
 numeros.forEach(n => {

2345e3dfe925c6915ab20f9f8eca8853e2bd034d
Matheus Fantini <matheusfantini@gmail.com>
2022-07-27 21:35:37 -0300

    Update exerciciosListas3.js

--- exerciciosDePythonEmJs/exerciciosListas3.js
+++ exerciciosDePythonEmJs/exerciciosListas3.js
@@ -2,8 +2,8 @@
 // [nota1,nota2,nota3,nota4] = [4, 2, 5, 4]
 // console.log( `suas 4 notas são: ${[nota1 ,nota2 ,nota3 ,nota4]} e a sua média é: ${([nota1 +nota2 +nota3 +nota4])/4} `)
 
-notas = [4, 5, 8, 10]
-mediaC=0
+const notas = [4, 5, 8, 10]
+let mediaC=0
 notas.forEach(nota => {
     mediaC += nota/notas.length
     

6159be8a91af73907b8d7b4dddf7ce9568c534de
Matheus Fantini <matheusfantini@gmail.com>
2022-07-27 21:35:34 -0300

    Update exerciciosFunções3.js

--- exerciciosDePythonEmJs/exerciciosFunções3.js
+++ exerciciosDePythonEmJs/exerciciosFunções3.js
@@ -2,5 +2,5 @@ function somaValor(ValorA, ValorB, ValorC) {
     return ValorA+ValorB+ValorC
 }
 
-ValorSomado = somaValor(10, 50, 30)
+let ValorSomado = somaValor(10, 50, 30)
 console.log(ValorSomado)

965df1cf1e9f972c2c6da8500d8c13f80d12b4ee
Matheus Fantini <matheusfantini@gmail.com>
2022-07-27 21:35:48 -0300

    Update exerciciosRepetições17.js

--- exerciciosDePythonEmJs/exerciciosRepetições17.js
+++ exerciciosDePythonEmJs/exerciciosRepetições17.js
@@ -1,7 +1,7 @@
 //Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120
 
-num = 3
-fatorial = 1
+let num = 3
+let fatorial = 1
 
 for (i = num; num > 0; num--) {
     fatorial = num * fatorial

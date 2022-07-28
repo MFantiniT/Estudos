342768dcdd7886bf9b3c6aa75ee19e2f9558cdbf
Matheus Fantini <matheusfantini@gmail.com>
2022-07-28 17:01:05 -0300

    Update exerciciosRepetições21.js

--- exerciciosDePythonEmJs/exerciciosRepetições21.js
+++ exerciciosDePythonEmJs/exerciciosRepetições21.js
@@ -2,17 +2,7 @@ let num = 11
 for (i = 1; i <= num; i++)
     lista = []
 let divisor = 0
-// lista.forEach(multiplicador => {
-//     if(multiplicador%num==0)
-//     divisor+=1    
 
-// });
-// if(divisor==2){
-//     console.log(`o numero ${num} é primo.`)
-// }
-// else{
-//     console.log(`o número ${num} não é primo`)
-// }
 function primos(num){
     for (i = 1; i <= num; i++) {
         //console.log(num % i)

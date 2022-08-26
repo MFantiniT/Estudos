9fb75008f1b660d72c59b3e53863430de28c2de1
Matheus Fantini <matheusfantini@gmail.com>
2022-08-26 08:24:42 -0300

    Create exerciciosListaRrodrigo1.js

---
+++ exerciciosDePythonEmJs/exerciciosListaRrodrigo1.js
@@ -0,0 +1,34 @@
+let data = new Date();
+console.log(data)
+let dia = data.getDay();
+console.log(dia)
+let listaDias = ["Segunda", "Terça", "Quarta", "Quinta ", "Sexta", "Sábado", "Domingo"];
+console.log("Ho.je é : " + listaDias[dia] + ".");
+let horas = data.getHours();
+console.log(horas)
+let minutos = data.getMinutes();
+let segundos = data.getSeconds();
+// let diaNoite = (horas >= 12) ? " PM " : " AM ";
+// horas = (horas >= 12) ? horas - 12 : horas;
+// if (horas === 0 && diaNoite === ' PM ') {
+//     if (minutos === 0 && segundos === 0) {
+//         horas = 12;
+//         diaNoite = ' Meio dia';
+//     }
+//     else {
+//         horas = 12;
+//         diaNoite = ' PM';
+//     }
+// }
+// if (horas === 0 && diaNoite === ' AM ') {
+//     if (minutos === 0 && segundos === 0) {
+//         horas = 12;
+//         diaNoite = ' Meia noite';
+//     }
+//     else {
+//         horas = 12;
+//         diaNoite = ' AM';
+//     }
+// }
+// console.log("horas : " + horas + diaNoite + " : " + minutos + " : " + segundos);
+console.log("horas : " + horas + "H : " + minutos + "M : " + segundos + "S");

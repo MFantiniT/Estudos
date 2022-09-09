2d1058f27ac66bac19a0cb422aa17ec977d445ad
Matheus Fantini <matheusfantini@gmail.com>
2022-09-08 21:05:38 -0300

    Create exercicioRodrigo4.js

---
+++ exercicioRodrigo4.js
@@ -0,0 +1,11 @@
+var lado = 5
+var lado2 = 6
+var lado3 = 7
+var somaDosLados = lado + lado2 + lado3;
+const perimetro = somaDosLados / 2
+
+area = Math.sqrt(((perimetro - lado) * (perimetro - lado2) * (perimetro - lado3)) * perimetro)
+
+console.log(area)
+
+

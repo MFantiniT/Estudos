2a5891fede6f9b49760c51c1940b9265e771d1f7
Matheus Fantini <matheusfantini@gmail.com>
2022-09-13 19:45:49 -0300

    exerciciosRodrigo

---
+++ exerciciosDePythonEmJs/exercicio11Rodrigo.js
@@ -0,0 +1,12 @@
+c = 29
+f = 84.2
+conversorC_F = (1.8 * c) + 32
+
+conversorF_C = ((f -32) * 5)/9
+
+
+console.log(`temperatura em graus Celsius ${c}
+temperatura em graus fahrenheit ${conversorC_F}`)
+
+console.log(`temperatura em graus fahrenheit ${f}
+temperatura em graus celsius ${conversorF_C}`)

e85da1f2499fa28659927efa0e1fbb14a3e91ab2
Matheus Fantini Teixeira <matheusfantini@gmail.com>
2022-10-04 18:50:58 -0300

    treino

---
+++ exerciciosDePythonEmJs/CRUD.js
@@ -0,0 +1,20 @@
+const miniTwitter = {
+    usuario: [{
+        username: 'matheusF',
+    }
+],
+    posts: [
+        {
+            proprietario: 'matheusF',
+            conteudo: 'Meu Tweet',
+
+        }
+],
+}
+    function criaPost(proprietario, conteudo){
+        this.proprietario = proprietario
+        this.conteudo = conteudo
+    }
+
+console.log(miniTwitter.posts)    
+
---
+++ exerciciosDePythonEmJs/Untitled-1.js
@@ -0,0 +1,20 @@
+const miniTwitter = {
+    usuarios : [
+        {
+            username: '_matheusfantini'
+        }
+    ],
+    posts: [
+        {
+            owner: '_matheusfantini',
+            content: 'meu tweet',
+        },
+        function criaPost(dados) {
+            dados.owner = owner
+            dados.content = content
+            
+        }
+    ]
+console.log("oi")
+
+}
